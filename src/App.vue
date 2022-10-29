a3d34359e4448b0ee85b0b5cc70bb383d0245632
Dan <amorecbellum@gmail.com>
2022-10-29 03:05:45 -0500

    Refactored Data a little

--- src/App.vue
+++ src/App.vue
@@ -71,63 +71,11 @@ import {useQuasar} from "quasar";
 import DialogueBox from "@/components/DialogueBox/DialogueBox";
 import CategoryInput from "@/components/DialogueBox/CategoryInput";
 import FeatureInput from "@/components/DialogueBox/FeatureInput";
-import {ELEMENT_TYPE, FeatureDataController} from '@/components/appData'
+
 import CategoryComponentItem from "@/components/CategoryComponentItem";
+import {ELEMENT_TYPE, importData} from "@/components/scripts/appdata";
 
-const dataController = new FeatureDataController()
-dataController.features
-    .create(
-        'New Challenge to Minecraft!',
-        "You are now a guardian spirit here to watch over a spirit tree! It is your job to care" +
-        " for and grow the spirit tree to becomes a monument amongst trees! \n" +
-        "\n" +
-        "This mod turns every player into a dryad who, after choosing a tree to protect, binds them to " +
-        "the tree. Wandering too far away will deplete a player's air while being close to the tree will " +
-        "fill it back up.",
-        0,
-        0,
-    )
-    .create(
-        'Choosing the tree',
-        "When a player joins, they will start in ghost form. In order to get out of ghost form," +
-        "a player must chose a tree by punching it and accepting the chat prompt. " +
-        "\n\n" +
-        "Once chosen, a player is then given a few saplings to start and must then grow the tree by hand.",
-        0,
-        1,
-    )
-    .create(
-        'Growing Stick',
-        "To grow the tree, the player must add only logs that are descended from the original" +
-        "tree. Saplings from the player's spirit tree will glow and be specially named. When planted, " +
-        "these trees will produce logs that glow when picked up by the guardian player. " +
-        "\n\n" +
-        "Using logs or saplings from other trees will not grow your spirit tree, though they can be used " +
-        "as decoration. Placed logs that are part of the spirit tree will pulse when holding a Log or " +
-        "sappling from the tree.",
-        2,
-        2,
-    )
-    .create(
-        'Home upgrade: Reach',
-        "This upgrade allows a player to go farther from the tree by two blocks per level." +
-        "\n\n" +
-        "Requirements: A tree must have at least 200 blocks for this upgrade to unlock. It costs one enderpearl," +
-        "one magmablock, and 10 experience levels per upgrade with a max of 3 upgrades",
-        1,
-        3,
-    )
-
-dataController.categories
-    .create(
-        'Core Gameplay',
-        0,
-    )
-    .create(
-        'Upgrades',
-        1,
-        1,
-    )
+const dataController = importData()
 
 const typeDirector = {}
 typeDirector[ELEMENT_TYPE.CATEGORY] = {
@@ -144,7 +92,6 @@ export default {
   components: {
     FeatureComponent,
     CategoryComponentItem
-
   },
   setup() {
     const DialogController = function () {
